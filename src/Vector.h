a8715d20872627a922df4680076211996f31f4bc
Lotto York <lottoyork@gmail.com>
2026-08-04 16:07:27 +0700

    _____temporary update

--- src/Vector.h
+++ src/Vector.h
@@ -1,9 +1,70 @@
+#include <stdexcept>
+#include <string>
+#include <list>
+#include <iostream>
+using namespace std;
+
 template<typename T>
 class Vector {
 private:
     T* elem; // elem points to an array of sz elements of type T
     int sz;  // number of elements
 public:
-    Vector(int s) : elem{new T[s]}, sz{s} {} // constructor
+    Vector(int s); // constructor
     ~Vector() { delete[] elem; } // destructor
     
+    // move constructor & move assignment operator
+    // Vector(Vector&&) noexcept;
+    // Vector& operator=(Vector&&) noexcept;
+
+    T& operator[](int i); // access: return reference
+    const T& operator[](int i) const; // access: return const reference
+    int size() const { return sz; } // return number of elements
+};
+
+class Negative_size : public std::length_error {
+public:
+    Negative_size() : std::length_error{"Vector constructor: negative size"} {}
+};
+
+template<typename T>
+Vector<T>::Vector(int s)
+{
+    if (s < 0) throw Negative_size{};
+    elem = new T[s];
+    sz = s;
+}
+
+template<typename T>
+T& Vector<T>::operator[](int i)
+{
+    if(i < 0 || i >= sz) 
+        throw out_of_range{"Vector::operator[]: index out of range"};
+    return elem[i];
+}
+
+template<typename T>
+const T& Vector<T>::operator[](int i) const
+{
+    if(i < 0 || i >= sz) 
+        throw out_of_range{"Vector::operator[]: index out of range"};
+    return elem[i];
+}
+
+// template<typename T>
+// Vector<T>::Vector(Vector&& a) noexcept : elem{a.elem}, sz{a.sz} {
+//     a.elem = nullptr;
+//     a.sz = 0;
+// }
+
+// template<typename T>
+// Vector<T>& Vector<T>::operator=(Vector&& a) noexcept {
+//     if (this != &a) {
+//         delete[] elem;
+//         elem = a.elem;
+//         sz = a.sz;
+//         a.elem = nullptr;
+//         a.sz = 0;
+//     }
+//     return *this;
+// }
---
+++ src/main.cpp
@@ -0,0 +1,110 @@
+#include "Vector.h"
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+enum class VectorType { char_vector, string_vector, list_int_vector };
+
+VectorType get_vector_type(istream& is) {
+    char ch;
+    if (!(is >> ch)) {
+        throw runtime_error("No input for vector type");
+    }
+
+    switch (ch) {
+        case 'c': return VectorType::char_vector;
+        case 's': return VectorType::string_vector;
+        case 'l': 
+            if (!(is >> ch) || ch != 'i') {
+                throw runtime_error("Expected 'li' for list<int> vector type");
+            }
+            return VectorType::list_int_vector;
+        default: 
+            char ch2;
+            is >> ch2; // Read the next character to provide more context in the error message
+            cout << "Unknown vector type: " << ch << ch2 << endl;
+            throw runtime_error("Unknown vector type");
+    }
+}
+
+void write(const Vector<char>& vc)
+{
+    for (int i = 0; i < vc.size(); ++i)
+        cout << vc[i];
+    cout << endl;
+}
+
+void write(const Vector<string>& vs)
+{
+    for (int i = 0; i < vs.size(); ++i)
+        cout << vs[i] << '\n';
+}
+
+void write(const Vector<list<int>>& vli)
+{
+    for (int i = 0; i < vli.size(); ++i) {
+        cout << "List " << i << ": ";
+        for (const auto& val : vli[i]) {
+            cout << val << ' ';
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    Vector<char> vc(200);       // vector of 200 characters
+    Vector<string> vs(17);      // vector of 17 strings
+    Vector<list<int>> vli(45);  // vector of 45 lists of integers
+
+    while(cin && !cin.eof()) {
+        try {
+            VectorType vt = get_vector_type(cin);
+            int size;
+            cin >> size;
+
+            switch (vt) {
+                case VectorType::char_vector: {
+                    for (int i = 0; i < size; ++i) {
+                        cin >> vc[i];
+                    }
+                    cout << "Read char vector of size " << size << endl;
+                    continue; // Skip to the next iteration after reading char vector
+                }
+                case VectorType::string_vector: {
+                    for (int i = 0; i < size; ++i) {
+                        cin >> vs[i];
+                    }
+                    cout << "Read string vector of size " << size << endl;
+                    continue; // Skip to the next iteration after reading string vector
+                }
+                case VectorType::list_int_vector: {
+                    for (int i = 0; i < size; ++i) {
+                        int list_size;
+                        cin >> list_size;
+                        list<int> li;
+                        for (int j = 0; j < list_size; ++j) {
+                            int value;
+                            cin >> value;
+                            li.push_back(value);
+                        }
+                        vli[i] = li;
+                    }
+                    cout << "Read list<int> vector of size " << size << endl;
+                    continue; // Skip to the next iteration after reading list<int> vector
+                }
+                default:
+                    throw runtime_error("Unhandled vector type");
+            }
+        } catch (const exception& e) {
+            cerr << "Error reading char vector: " << e.what() << endl;
+            break;
+        }
+    }
+
+    write(vc);
+    write(vs);
+    write(vli);
+
+    return 0;
+}
